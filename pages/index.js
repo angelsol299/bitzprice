0bbd65000fb74eb44f8e883a8a4bc592989e6c72
angel <angelsol299@hotmail.com>
2019-04-09 23:07:52 +0200

    fisrt components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+import Link from "next/link";
+
 const Index = () => (
   <div>
     {" "}
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
     <h1>welcome to bitzprice</h1>
   </div>
 );
