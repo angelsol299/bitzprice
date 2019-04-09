4f039921e0fad583184254aac84a0d50b8616a64
angel <angelsol299@hotmail.com>
2019-04-09 22:39:47 +0200

    fisrt components

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => (
+  <div>
+    {" "}
+    <h1>About bitzprice</h1>
+    <p>Application to view Bitcoin prices</p>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = () => (
+  <div>
+    {" "}
+    <h1>welcome to bitzprice</h1>
+  </div>
+);
+
+export default Index;
